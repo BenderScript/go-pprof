1d8f0b37dd9405922df6b0f3530960a76b1c4a96
repenno <rapenno@gmail.com>
2020-11-27 19:49:11 -0800

    Separating Pprof and main server

--- main.go
+++ main.go
@@ -1,57 +1,61 @@
 package main
 
 import (
+	"context"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
-	"net/http/pprof"
 	"os"
 	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 
-	pprofRouter := mux.NewRouter()
-	AddPprofRoutes(pprofRouter)
+	termPprof := make(chan bool)
+	pprofDone := make(chan bool)
+	done := make(chan bool)
 
-	pprofServer := &http.Server{
-		Handler:      pprofRouter,
-		Addr:         "127.0.0.1:15121",
+	InitPprof(termPprof, pprofDone)
+
+	newRouter := mux.NewRouter()
+
+	server := &http.Server{
+		Handler:      newRouter,
+		Addr:         "127.0.0.1:15120",
 		WriteTimeout: 100 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
 
-	// done := make(chan bool)
 	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		log.Println("=============================================")
-		log.Printf("==== Starting PProf at: %s =====\n", pprofServer.Addr)
-		if err := pprofServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("Could not listen on %s: %v", pprofServer.Addr, err)
+		log.Printf("==== Starting Server at: %s =====\n", server.Addr)
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not listen on %s: %v", server.Addr, err)
 		}
 	}()
 
-	<-quit
-}
+	go func() {
+		<-quit
+		log.Println("Server is shutting down...")
+		termPprof <- true
+		<-pprofDone
 
-// AddPprofRoutes
-func AddPprofRoutes(r *mux.Router) {
-	debugProf := r.PathPrefix("/debug/pprof").Subrouter()
-	debugProf.HandleFunc("/", pprof.Index)
-	debugProf.HandleFunc("/cmdline", pprof.Cmdline)
-	debugProf.HandleFunc("/symbol", pprof.Symbol)
-	debugProf.HandleFunc("/trace", pprof.Trace)
-	debugProf.HandleFunc("/profile", pprof.Profile)
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
 
-	// Manually add support for paths not easily linked as above
-	// Hooking this up is actually very convoluted and only a few answers on how to do it
-	// https://stackoverflow.com/questions/19591065/profiling-go-web-application-built-with-gorillas-mux-with-net-http-pprof
-	// Alternatively one can go to localhost:6060
-	debugProf.Handle("/goroutine", pprof.Handler("goroutine"))
-	debugProf.Handle("/heap", pprof.Handler("heap"))
-	debugProf.Handle("/threadcreate", pprof.Handler("threadcreate"))
-	debugProf.Handle("/block", pprof.Handler("block"))
+		server.SetKeepAlivesEnabled(false)
+		if err := server.Shutdown(ctx); err != nil {
+			log.Fatalf("Could not gracefully shutdown the server: %v", err)
+		}
+		close(done)
+	}()
+
+	<-done
+	log.Println("Server stopped")
+
 }
---
+++ pprof.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"net/http/pprof"
+	"time"
+)
+
+func InitPprof(termPprof, pprofDone chan bool) {
+
+	pprofRouter := mux.NewRouter()
+	AddPprofRoutes(pprofRouter)
+
+	pprofServer := &http.Server{
+		Handler:      pprofRouter,
+		Addr:         "127.0.0.1:15121",
+		WriteTimeout: 100 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	go func() {
+		log.Println("=============================================")
+		log.Printf("==== Starting PProf at: %s =====\n", pprofServer.Addr)
+		if err := pprofServer.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Could not listen on %s: %v", pprofServer.Addr, err)
+		}
+	}()
+
+	go func() {
+		<-termPprof
+		log.Println("Pprof Server is shutting down...")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		pprofServer.SetKeepAlivesEnabled(false)
+		if err := pprofServer.Shutdown(ctx); err != nil {
+			log.Fatalf("Could not gracefully shutdown the server: %v", err)
+		}
+		pprofDone <- true
+		log.Println("Pprof Server stopped")
+	}()
+}
+
+// AddPprofRoutes
+func AddPprofRoutes(r *mux.Router) {
+	debugProf := r.PathPrefix("/debug/pprof").Subrouter()
+	debugProf.HandleFunc("/", pprof.Index)
+	debugProf.HandleFunc("/cmdline", pprof.Cmdline)
+	debugProf.HandleFunc("/symbol", pprof.Symbol)
+	debugProf.HandleFunc("/trace", pprof.Trace)
+	debugProf.HandleFunc("/profile", pprof.Profile)
+
+	// Manually add support for paths not easily linked as above
+	// Hooking this up is actually very convoluted and only a few answers on how to do it
+	// https://stackoverflow.com/questions/19591065/profiling-go-web-application-built-with-gorillas-mux-with-net-http-pprof
+	// Alternatively one can go to localhost:6060
+	debugProf.Handle("/goroutine", pprof.Handler("goroutine"))
+	debugProf.Handle("/heap", pprof.Handler("heap"))
+	debugProf.Handle("/threadcreate", pprof.Handler("threadcreate"))
+	debugProf.Handle("/block", pprof.Handler("block"))
+}
